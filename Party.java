37a6674be2f89f2a68b1340dead665c4b653bdc3
Himar14 <87143982+Himar14@users.noreply.github.com>
2021-07-15 09:56:39 -0600

    Update Party.java

--- Party.java
+++ Party.java
@@ -13,9 +13,11 @@ public class Party {
 	}
 	public Party(String partyName, float projectedNumberOfSeats, float projectedPercentageOfVotes) {
 		name = partyName;
+		// code below restricts the projectedNumberOfSeats to being above zero( non negative value). If so, the entered value can equal the variable.
 		if (projectedNumberOfSeats > 0) {
 			this.projectedNumberOfSeats = projectedNumberOfSeats;
 		}
+		// code below restricts the projectedPercentageofVotes to being above  or equal to zero and less than or equal to one. If so, the entered value can equal the variable.
 		if (projectedPercentageOfVotes >= 0 && projectedPercentageOfVotes <= 1){
 			this.projectedPercentageOfVotes = projectedPercentageOfVotes;
 		}
@@ -31,6 +33,7 @@ public class Party {
 	}
 	
 	public void setProjectedPercentageOfVotes(float projectedPercentageOfVotes) {
+		// ensures the value falls between and including zero and one.
 		if (projectedPercentageOfVotes >= 0 && projectedPercentageOfVotes <= 1) {
 			this.projectedPercentageOfVotes = projectedPercentageOfVotes;
 		}
@@ -61,13 +64,17 @@ public class Party {
 	}
 	
 	public String textVisualizationBySeats(int maxStars, int starsNeededForMajority, double numOfSeatsPerStar) {
+	// converts the projectedPercentageOfVotes to a percentage.
 	    int percentage = (int) (projectedPercentageOfVotes * 100);
+	// determines the amount of stars based on the projectedNumberOfSeats.
 	    int percentageRatio = (int)(projectedNumberOfSeats/numOfSeatsPerStar);
+	 //Utilizes the byBoth method to execute the printing of stars based on their percentageRatio(which are different for both methods)
 	    String space = byBoth (maxStars,starsNeededForMajority,percentageRatio);
 		return space + " " + name +  " (" + percentage + "% of votes, " + projectedNumberOfSeats + " seats)";	}
 
 	public String textVisualizationByVotes(int maxStars, int starsNeededForMajority, double percentOfVotesPerStar) {
-		int percentage = (int) (projectedPercentageOfVotes * 100);
+	    int percentage = (int) (projectedPercentageOfVotes * 100);
+	// determines the amount stars that needs to be printed based on the percentage of votes received and how many of those votes go in one star.
 	    int percentageRatio = (int)(percentage/percentOfVotesPerStar);
 	    String space = byBoth (maxStars,starsNeededForMajority,percentageRatio);;
 		return space + " " + name +  " (" + percentage + "% of votes, " + projectedNumberOfSeats + " seats)";	}
