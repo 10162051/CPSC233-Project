ae87c97011253ec6e2dcdce28d86686abe63cf9c
XiaShaohuan <shaohuan.xia@ucalgary.ca>
2021-07-30 17:35:33 -0400

    Structure changed.

--- application/Main.java
+++ application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			BorderPane root = loader.load(new FileInputStream("application/TheWindow.fxml"));
+			BorderPane root = loader.load(new FileInputStream("application/EditPollView.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
