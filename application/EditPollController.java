e71ecc4a516ccf9c3dc65a90b90acce4d7466837
XiaShaohuan <shaohuan.xia@ucalgary.ca>
2021-07-30 23:06:32 -0400

    Structure updated.

--- application/EditPollController.java
+++ application/EditPollController.java
@@ -1,5 +1,77 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
 public class EditPollController {
 
+    @FXML
+    private Label ProjectedNumberOfSeat;
+
+    @FXML
+    private TextField ProjectedNumberOfSeatText;
+
+    @FXML
+    private Label ProjectedPercentageOfTheVote;
+
+    @FXML
+    private Button UpdatePollNameButton;
+
+    @FXML
+    private TextField UpdatePollNameText;
+
+    @FXML
+    private Button ClearButton;
+
+    @FXML
+    private HBox Hbox1;
+
+    @FXML
+    private HBox Hbox2;
+
+    @FXML
+    private BorderPane Pane;
+
+    @FXML
+    private Label PercentageMark;
+
+    @FXML
+    private Label per290;
+
+    @FXML
+    private ChoiceBox<?> EditChoiceBox;
+
+    @FXML
+    private HBox Hbox3;
+
+    @FXML
+    private HBox Hbox4;
+
+    @FXML
+    private ChoiceBox<?> PartyToUpdateChoiceBox;
+
+    @FXML
+    private HBox Hbox5;
+
+    @FXML
+    private HBox Hbox6;
+
+    @FXML
+    private Label PollToEdit;
+
+    @FXML
+    private TextField ProjectedPercentageOfTheVoteText;
+
+    @FXML
+    private Button UpdatePartyButton;
+
+    @FXML
+    private Label PartyToUpdate;
+
 }
+
