897b4493f33b028bf1cf8a4552b30f8a426ca57f
C.Kalin <87056237+10162051@users.noreply.github.com>
2021-08-05 16:08:14 -0600

    Choice Box now functional. Pie charts not up yet.

--- application/VisualizePollController.java
+++ application/VisualizePollController.java
@@ -1,23 +1,38 @@
 package application;
 
+//ALWAYS IMPORT FROM JAVAFX. CHECK WHEN YOU IMPORT
 import java.util.Random;
-
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import model.Poll;
+import model.PollList;
+import model.Factory;
 
-
-// ALWAYS IMPORT FROM JAVAFX. CHECK WHEN YOU IMPORT
 public class VisualizePollController {
+	//Instance Variable
+	private PollList polls;
+	// Getter and Setter * We'll do something with these later
+	PollList getPolls() {
+		return polls;
+	}
+	void setPolls(PollList polls) {
+		this.polls = polls;
+	}
 
-    @FXML
-    private ChoiceBox<String> pollButton; //String can be polls, parties, etc. 
-    
+	@FXML
+    private ChoiceBox<Poll> pollButton; 
+	
     @FXML
     private Label selectPollLabel;
 
@@ -32,43 +47,40 @@ public class VisualizePollController {
 
     @FXML
     private PieChart precentVotesPieChart;
+    
+    @FXML
+    private Button displayResultsButton;
 
     @FXML
-    void openSelectPoll(MouseEvent event) {
-    	// THIS SHOULD BE WHEN A CHOICE IS SELECTED FROM THE DROP DOWN MENU.
-    	PieChart.Data[] data = new PieChart.Data[6]; //6 is a place setting
-    	Random rand = new Random();
-    	data [0] = new PieChart. Data("Poll 1", rand.nextInt(100));
-    	data [1] = new PieChart. Data("Poll 2", rand.nextInt(100));
-    	data [2] = new PieChart. Data("Poll 3", rand.nextInt(100));
-    	data [3] = new PieChart. Data("Poll 4", rand.nextInt(100));
-    	data [4] = new PieChart. Data("Poll 5", rand.nextInt(100));
-    	data [5] = new PieChart. Data("Poll 6", rand.nextInt(100));
-    	
-    	seatsPieChart.setData(FXCollections.observableArrayList(data));
-    }
-    @FXML
     void initialize() {
-        assert pollButton != null : "fx:id=\"pollButton\" was not injected: check your FXML file 'Assignment2.fxml'.";
+    	this.setPolls(Factory.getInstance().createRandomPollList());
+    	assert pollButton != null : "fx:id=\"pollButton\" was not injected: check your FXML file 'Assignment2.fxml'.";
         assert selectPollLabel != null : "fx:id=\"selectPollLabel\" was not injected: check your FXML file 'Assignment2.fxml'.";
         assert projectedSeatsLabel != null : "fx:id=\"projectedSeatsLabel\" was not injected: check your FXML file 'Assignment2.fxml'.";
         assert projectedPercentVotesLabel != null : "fx:id=\"projectedPercentVotesLabel\" was not injected: check your FXML file 'Assignment2.fxml'.";
         assert seatsPieChart != null : "fx:id=\"seatsPieChart\" was not injected: check your FXML file 'Assignment2.fxml'.";
         assert precentVotesPieChart != null : "fx:id=\"precentVotesPieChart\" was not injected: check your FXML file 'Assignment2.fxml'.";
         
-        //For the drop down box
-        //String[] values = {"Aggregate Poll", "All Polls"};
-        //seatsPieChart.setItems(FXCollections.observableArrayList(values));
-        //seatsPieChart.getSelectModel().selectedIndexProperty().addListener(listener
-        		//new ChangeListener<Number>(){
-        		//@Override
-        			//public void changed(ObservableValue observable, Number oldValue, Number newValue){
-        			//int index = newValue.intValue();
-        		//}
-        //	}
-     //   );
+        //ChoiceBox
+        ObservableList<Poll> selectPollList;
+        pollButton.setItems(FXCollections.observableArrayList(this.polls.getPolls()));
+        pollButton.getSelectionModel().selectedIndexProperty().addListener(
+        	    new ChangeListener<Number>() {
+        	        @Override
+        	        public void changed(ObservableValue observable, Number oldValue, Number newValue) {
+        	            int value = newValue.intValue();
+        	        }
+        	    }
+        	);
     }
-
+    
+    private void setPieChartData(Poll aPoll) {
+    	//create and declare PieChart Data arrays, and ultimately set those as the values 
+    	//of the PieCharts that you have.
+    	// It should take the data within the Poll instance and use the votes and seats to set that data.
+    	ObservableList<Poll> pieChartPoll;
+    	
+    }
 }
 
 
@@ -82,9 +94,6 @@ public class VisualizePollController {
 // data[0] = new PieChart.Data("label here", rand.nextInt(101)); RAND.NEXTINT GENERATES RANDOM NUMBER FROM RANDOM RAND
 //Need 5 data[num] if we do it this way.
 //PieChart.setData(FXCollections.observableArrayList(data));
-
-//For the drop down box
-//String[] values = {"Aggregate Poll", "All Polls"};
 //ChoiceBox.setItems(FX.Collection.observableArrayList(values));
 //ChoiceBox.getSelectModel().selectedIndexProperty().addListener(listener
 //	 new ChangeListener<Number>(){
